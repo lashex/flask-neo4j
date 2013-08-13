ca0fc561786f59c21cd6c069c2daff302c32ef6d
Brett Francis <brett@gridward.com>
2013-08-13 00:26:09 -0700

    now have auto-index instantiation working

--- test_flask_neo4j.py
+++ test_flask_neo4j.py
@@ -1,5 +1,6 @@
 import flask
 import flask.ext.neo4j
+from py2neo import neo4j
 import unittest
 
 
@@ -13,15 +14,26 @@ class FlaskRequestTest(unittest.TestCase):
         self.context.pop()
 
 
+class FlaskNeo4jIndexTestCase(FlaskRequestTest):
+    def test_index_create(self):
+        test_indexes = {'Foo': neo4j.Node, 'BarRel': neo4j.Relationship}
+        n4j = flask.ext.neo4j.Neo4j(self.app, test_indexes)
+        assert n4j.index['Foo'] is not None
+        assert n4j.index['BarRel'] is not None
+
+
 class FlaskNeo4jConfigTestCase(FlaskRequestTest):
     def test_graph_db_config(self):
         self.app.config['GRAPH_DATABASE'] = 'http://localhost:7474/db/data/'
         n4j = flask.ext.neo4j.Neo4j(self.app)
+        assert n4j.gdb is not None
         assert n4j.gdb.neo4j_version is not None
+
 
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(FlaskRequestTest))
+    suite.addTest(unittest.makeSuite(FlaskNeo4jIndexTestCase))
     suite.addTest(unittest.makeSuite(FlaskNeo4jConfigTestCase))
     return suite
 
